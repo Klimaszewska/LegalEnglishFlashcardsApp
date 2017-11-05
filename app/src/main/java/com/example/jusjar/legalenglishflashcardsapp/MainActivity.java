903a1eb0332add0f00cf9bd734238611eebb9b6d
Klimaszewska <klimaszewska.justyna@gmail.com>
2017-11-05 10:09:48 +0100

    New activities created for all the screens

--- app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/MainActivity.java
+++ app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appBarLayout);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
---
+++ app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/WordPairs.java
@@ -0,0 +1,13 @@
+package com.example.jusjar.legalenglishflashcardsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordPairs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_pairs);
+    }
+}
