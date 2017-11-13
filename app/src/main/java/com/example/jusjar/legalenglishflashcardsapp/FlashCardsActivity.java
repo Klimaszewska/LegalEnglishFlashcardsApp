892c2b5bd084eb43424597f7d2247a300487f021
Klimaszewska <klimaszewska.justyna@gmail.com>
2017-11-13 17:50:36 +0100

    Flashcards activity updated (focusing on Checked button and Correct button).

--- app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/FlashCardsActivity.java
+++ app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/FlashCardsActivity.java
@@ -5,13 +5,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FlashCardsActivity extends AppCompatActivity {
+
+    // fields
+
+    private Button buttonCheck;
+    private Button buttonCorrect;
+    private TextView displayedWord;
+
+    private WordPairs[] words = new WordPairs[]{
+        new WordPairs(R.string.wordInput1, "Sample EN 1"),
+        new WordPairs(R.string.wordInput2, "Sample EN 2"),
+        new WordPairs(R.string.wordInput3, "Sample EN 3")
+    };
+
+    private int currentIndex = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_cards);
+
+        //wordInput = (TextView) findViewById(R.id.wordInput);
+
+        // call to the UI for textview
+        displayedWord = (TextView) findViewById(R.id.wordInput);
+
+        // call to the UI for the buttons
+        buttonCorrect = (Button) findViewById(R.id.buttonCorrect);
+        buttonCheck = (Button) findViewById(R.id.buttonCheck);
+        buttonCheck.setTag(1);
+        buttonCheck.setText("Check");
+
+        updateWordPair();
+
+        // onCLickListener for the correct button
+        buttonCorrect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                while(currentIndex<words.length) {
+                    updateWordPair();
+                    currentIndex++;
+                }
+            }
+        });
+
+        // onCLickListener for the check button
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int status = (Integer) v.getTag();
+                if (status == 1){
+                        buttonCheck.setText(words[currentIndex].getWordEn());
+                        v.setTag(0);
+                }else{
+                    buttonCheck.setText("Check");
+                    v.setTag(1);
+                }
+
+            }
+        });
+
 
         // TEMPORARY - TO BE REMOVED - test button to proceed to the next screen
         Button buttonWrong = (Button) findViewById(R.id.buttonWrong);
@@ -24,5 +82,10 @@ public class FlashCardsActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), FlashCardsFinalScreenActivity.class));
             }
         });
+    }
+
+    private void updateWordPair(){
+        int question = words[currentIndex].getWordPl();
+        displayedWord.setText(question);
     }
 }
--- app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/WordPairs.java
+++ app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/WordPairs.java
@@ -5,6 +5,34 @@ import android.os.Bundle;
 
 public class WordPairs extends AppCompatActivity {
 
+    // fields for words in Polish and words in English
+    private int wordPl;
+    private String wordEn;
+
+    // contructor created
+    public WordPairs(int wordPl, String wordEn) {
+        this.wordPl = wordPl;
+        this.wordEn = wordEn;
+    }
+
+    // getters and setters
+    public int getWordPl() {
+        return wordPl;
+    }
+
+    public void setWordPl(int wordPl) {
+        this.wordPl = wordPl;
+    }
+
+    public String getWordEn() {
+        return wordEn;
+    }
+
+    public void setWordEn(String wordEn) {
+        this.wordEn = wordEn;
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
