b2054a11f2e06379629144a07d1d01faca6113f8
Klimaszewska <klimaszewska.justyna@gmail.com>
2017-11-14 15:35:18 +0100

    All buttons working properly. TextViews have counters implemented.

--- app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/FlashCardsActivity.java
+++ app/src/main/java/com/example/jusjar/legalenglishflashcardsapp/FlashCardsActivity.java
@@ -1,6 +1,7 @@
 package com.example.jusjar.legalenglishflashcardsapp;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,13 @@ public class FlashCardsActivity extends AppCompatActivity {
 
     private Button buttonCheck;
     private Button buttonCorrect;
+    private Button buttonNotSure;
+
     private TextView wordInput;
+    private TextView numberCorrectText;
+    private TextView numberNotSureText;
+    private TextView numberWrongText;
+    private TextView questionsTotalText;
 
     private WordPairs[] words = new WordPairs[]{
         new WordPairs(R.string.wordInput1, "Sample EN 1"),
@@ -23,7 +30,10 @@ public class FlashCardsActivity extends AppCompatActivity {
     };
 
     private int currentIndex = 0;
-
+    private int numberCorrect = 0;
+    private int numberNotSure = 0;
+    private int numberWrong = 0;
+    private int questionsTotal = 0;
 
 
     @Override
@@ -37,12 +47,41 @@ public class FlashCardsActivity extends AppCompatActivity {
         wordInput = (TextView) findViewById(R.id.wordInput);
         wordInput.setText(words[currentIndex].getWordPl());
 
+        // call to the UI for score textviews
+        numberCorrectText = (TextView) findViewById(R.id.numberCorrect);
+        numberNotSureText = (TextView) findViewById(R.id.numberNotSure);
+        numberWrongText = (TextView) findViewById(R.id.numberWrong);
+        questionsTotalText = (TextView) findViewById(R.id.questionsTotal);
+
         // call to the UI for the buttons
         buttonCorrect = (Button) findViewById(R.id.buttonCorrect);
+        buttonNotSure = (Button) findViewById(R.id.buttonNotSure);
         buttonCheck = (Button) findViewById(R.id.buttonCheck);
         buttonCheck.setTag(0);
 
         updateWordPair();
+        setCorrectCounter();
+        setNotSureCounter();
+        setWrongCounter();
+        setQuestionsTotalCounter();
+
+        // onCLickListener for the check button
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //final int status = (Integer) v.getTag();
+                final int status = (Integer) v.getTag();
+                if (status == 1){
+                    buttonCheck.setText(words[currentIndex].getWordEn());
+                    v.setTag(0);
+                }else{
+                    buttonCheck.setText("Check");
+                    v.setTag(1);
+                }
+
+                //buttonCheck.setText(words[currentIndex].getWordEn());
+            }
+        });
 
         // onCLickListener for the correct button
         buttonCorrect.setOnClickListener(new View.OnClickListener() {
@@ -58,31 +97,35 @@ public class FlashCardsActivity extends AppCompatActivity {
                 if (currentIndex<words.length-1) {
                     currentIndex++;
                     updateWordPair();
-
-
+                    setCorrectCounter();
+                    setQuestionsTotalCounter();
                 }else{
-                    Toast.makeText(FlashCardsActivity.this, "Voila!", Toast.LENGTH_LONG).show();
+                    setCorrectCounter();
+                    setQuestionsTotalCounter();
+                    startActivity(new Intent(v.getContext(), FlashCardsFinalScreenActivity.class));
                 }
             }
         });
 
-        // onCLickListener for the check button
-        buttonCheck.setOnClickListener(new View.OnClickListener() {
+        // onCLickListener for the not sure button
+        buttonNotSure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //final int status = (Integer) v.getTag();
-                final int status = (Integer) v.getTag();
-                if (status == 1){
-                        buttonCheck.setText(words[currentIndex].getWordEn());
-                        v.setTag(0);
-                }else{
-                    buttonCheck.setText("Check");
-                    v.setTag(1);
-                }
 
-                //buttonCheck.setText(words[currentIndex].getWordEn());
+                if (currentIndex<words.length-1) {
+                    currentIndex++;
+                    updateWordPair();
+                    setNotSureCounter();
+                    setQuestionsTotalCounter();
+                }else{
+                    setNotSureCounter();
+                    setQuestionsTotalCounter();
+                    startActivity(new Intent(v.getContext(), FlashCardsFinalScreenActivity.class));
+                }
             }
         });
+
+
 
 
         // TEMPORARY - TO BE REMOVED - test button to proceed to the next screen
@@ -93,7 +136,16 @@ public class FlashCardsActivity extends AppCompatActivity {
         buttonWrong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), FlashCardsFinalScreenActivity.class));
+                if (currentIndex<words.length-1) {
+                    currentIndex++;
+                    updateWordPair();
+                    setWrongCounter();
+                    setQuestionsTotalCounter();
+                }else{
+                    setWrongCounter();
+                    setQuestionsTotalCounter();
+                    startActivity(new Intent(v.getContext(), FlashCardsFinalScreenActivity.class));
+                }
             }
         });
     }
@@ -106,5 +158,25 @@ public class FlashCardsActivity extends AppCompatActivity {
         buttonCheck.setText("Check");
         buttonCheck.setTag(1);
 
+    }
+
+    private void setCorrectCounter(){
+        numberCorrectText.setText("Correct: " + numberCorrect + "/" + words.length);
+        numberCorrect++;
+    }
+
+    private void setNotSureCounter(){
+        numberNotSureText.setText("Not sure: " + numberNotSure + "/" + words.length);
+        numberNotSure++;
+    }
+
+    private void setWrongCounter(){
+        numberWrongText.setText("Wrong: " + numberWrong + "/" + words.length);
+        numberWrong++;
+    }
+
+    private void setQuestionsTotalCounter(){
+        questionsTotalText.setText("Question: " + questionsTotal + "/" + words.length);
+        questionsTotal++;
     }
 }
